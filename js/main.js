9ab2c63753e521641e14d6198027bf6234795ff9
Katya Mullendore <ekmullendore@gmail.com>
2016-02-11 08:41:31 -0600

    Module 4 - not working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-main.js
+ var map = L.map('map').setView([16, 23], 6);
+ L.tileLayer('https://b.tiles.mapbox.com/v4/emullendore.p4d6m6dj/6/16/23.png?access_token=pk.eyJ1IjoiZW11bGxlbmRvcmUiLCJhIjoiY2lraG54bTI2MDIzb3VjajdtZzAyYWhpbSJ9.r-Q0c0qgDzXAx1Rpkw_aoA', {
+   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+   maxZoon: 18,
+   id: 'emullendore.p4d6m6dj',
+  accessToken: 'pk.eyJ1IjoiZW11bGxlbmRvcmUiLCJhIjoiY2lraG54bTI2MDIzb3VjajdtZzAyYWhpbSJ9.r-Q0c0qgDzXAx1Rpkw_aoA'
+}).addTo(map);
