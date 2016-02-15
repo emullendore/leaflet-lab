6f086a9607d63cfbd9b16e45b91d8088a00bbffb
Katya Mullendore <ekmullendore@gmail.com>
2016-02-15 16:34:44 -0600

    Module 4 Activity Completed

--- js/airqual.js
+++ js/airqual.js
@@ -1,15 +1,21 @@
+
+//for my Lab 1, still doesn't show up on the webpage and I can't figure out why
+//nothing for console log either and it appears correctly conntected with index.html
 function createMap(){
+
   var map=L.map('map3', {
+
     center:[-20,30],
     zoom: 2
-    console.log(map3);
   });
-L.tilelayer('https://api.tiles.mapbox.com/v4/emullendore.p5gjble8/{z}/{x}/{y}.png?access_token=tk.eyJ1IjoiZW11bGxlbmRvcmUiLCJleHAiOjE0NTU1MDQ4MTUsImlhdCI6MTQ1NTUwMTIxNCwic2NvcGVzIjpbImVzc2VudGlhbHMiLCJzY29wZXM6bGlzdCIsIm1hcDpyZWFkIiwibWFwOndyaXRlIiwidXNlcjpyZWFkIiwidXNlcjp3cml0ZSIsInVwbG9hZHM6cmVhZCIsInVwbG9hZHM6bGlzdCIsInVwbG9hZHM6d3JpdGUiLCJzdHlsZXM6dGlsZXMiLCJzdHlsZXM6cmVhZCIsImZvbnRzOnJlYWQiLCJzdHlsZXM6d3JpdGUiLCJzdHlsZXM6bGlzdCIsInN0eWxlczpkcmFmdCIsImZvbnRzOmxpc3QiLCJmb250czp3cml0ZSIsImZvbnRzOm1ldGFkYXRhIiwiZGF0YXNldHM6cmVhZCIsImRhdGFzZXRzOndyaXRlIl0sImNsaWVudCI6Im1hcGJveC5jb20ifQ.3j5JD4yfm35ZI-jkaovXtg&update=iknbu', {
-  attirbution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+L.tilelayer('https://a.tiles.mapbox.com/v4/emullendore.p5gjble8/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZW11bGxlbmRvcmUiLCJhIjoiY2lranhtcXQ4MDkya3Z0a200aGk0ZGRzMyJ9.zybsg8x8T8dh7mOgqTLiTg', {
+  attirbution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'emullendore.p5gjble8',
+  accessToken:'pk.eyJ1IjoiZW11bGxlbmRvcmUiLCJhIjoiY2lranhtcXQ4MDkya3Z0a200aGk0ZGRzMyJ9.zybsg8x8T8dh7mOgqTLiTg'
 }).addTo(map);
   getData(map);
 };
-
+console.log(map);
 function getData(map){
   $.ajax('data/airqual.geojson', {
     dataType: "json",
@@ -22,7 +28,7 @@ function getData(map){
         opacity: 1,
         fillOpacity: 1.0
       };
-      L.geojson(response,
+      L.geojson(response, {
       pointToLayer: function(feature, latlng) {
         return L.circleMarker(latlng, geojsonMarkerOptions);
       }
@@ -32,3 +38,4 @@ function getData(map){
   })
 };
 $(document).ready(createMap);
+console.log("hi");
--- js/geojson.js
+++ js/geojson.js
@@ -1,20 +1,24 @@
 //$('#map2').html("Practice Map");
 
 function createMap(){
+  //create map, assign to id 'map2'
   var map=L.map('map2', {
+    //set center, zoom
     center: [0,30],
     zoom: 2
   });
+//source of tile layer
 L.tileLayer('https://a.tiles.mapbox.com/v4/emullendore.p50l7ndp/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZW11bGxlbmRvcmUiLCJhIjoiY2lranhtcXQ4MDkya3Z0a200aGk0ZGRzMyJ9.zybsg8x8T8dh7mOgqTLiTg',{
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 }).addTo(map);
-
+  //within createMap function getData command
   getData(map);
 };
-
+//getData will use ajax to to access geojson filter
 function getData(map){
   $.ajax('data/MegaCities.geojson', {
     dataType: "json",
+    //with success, function will give geojson marker options in appearence
     success: function(response){
       var geojsonMarkerOptions={
         radius:4,
@@ -24,7 +28,7 @@ function getData(map){
         opacity: 1,
         fillOpacity: 1.0
       };
-      //pointToLayer stuff, geojsonMarkerOptions
+      //points will be assigned to a layers, instead of simple markers created
       L.geoJson(response, {
         pointToLayer: function (feature, latlng) {
           return L.circleMarker(latlng, geojsonMarkerOptions);
@@ -33,4 +37,5 @@ function getData(map){
 }
 })
 };
+//add map to document
 $(document).ready(createMap);
